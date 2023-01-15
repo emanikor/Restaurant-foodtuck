67f75a9819817fcb0b2ed826b6064c08f8c43278
emanikor <87859876+emanikor@users.noreply.github.com>
2023-01-15 16:33:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+
+//toggle button for navbar
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('active');
+
   
 })
 
-
+//foodmenu button---
 const menuBtns = document.querySelectorAll('.menu-btn');
 const foodItems = document.querySelectorAll('.food-item');
 
@@ -38,3 +41,47 @@ function showFoodMenu(newMenuBtn){
         }
     });
 }
+
+
+//subscribing button
+var subscribe_button = document.querySelector(".subscribe_button");
+
+subscribe_button.addEventListener('click', function(){
+	var subscribing = document.querySelector(".subscribing");
+	var thanks = document.querySelector(".thanks");
+	var login = document.querySelector(".login");
+
+	subscribing.classList.add("subscribing_active");
+	subscribe_button.classList.add("subscribe_button_active");
+	setTimeout(function(){
+		login.classList.add("login_active");
+	}, 1200);
+	setTimeout(function(){
+		thanks.classList.add("thanks_active");
+	}, 1400);
+
+	setTimeout(function(){
+		thanks.classList.remove("thanks_active");
+		login.classList.remove("login_active");
+		subscribing.classList.remove("subscribing_active");
+		subscribe_button.classList.remove("subscribe_button_active");
+	}, 4000);
+});
+// // Wrap every letter in a span
+// var textWrapper = document.querySelector('.ml6 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml6 .letter',
+//     translateY: ["1.1em", 0],
+//     translateZ: 0,
+//     duration: 750,
+//     delay: (el, i) => 50 * i
+//   }).add({
+//     targets: '.ml6',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
